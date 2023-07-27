3a5d6f6288162764b522ad2b1b61f4ea711e1f4e
Sabbbir <sabbir.ahmed1925@gmail.com>
2023-07-27 21:09:04 +0600

    Added first day's solve NumberMirror.java

---
+++ NumberMirror.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class NumberMirror{
+public static void main(String[] args) {
+    
+    int i;
+    Scanner scanner = new Scanner(System.in);
+    i = scanner.nextInt();
+    System.out.println(i);
+}
+
+}
