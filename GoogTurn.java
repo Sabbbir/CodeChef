0d8e3d6f64d020db4cddbbe84016a73511e20ee5
sabbbir <sabbir.ahmed1925@gmail.com>
2023-07-27 20:56:28 +0600

    Added first day's solve Number Mirror

---
+++ GoogTurn.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GoogTurn {
+    public static void main(String[] args) {
+        int i,j,sum = 0 ;
+        Scanner scanner = new Scanner(System.in);
+        j = scanner.nextInt();
+        for (i = 0; i <j;i++){
+
+            for(int k=0;k<2;k++){
+                        //take 2 inp
+                        //inp
+                        sum += arr[k];
+
+            }
+        }
+
+
+
+}
+}
