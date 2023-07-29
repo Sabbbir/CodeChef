dd18ab2b3fcf5d3bc4c3b66324dfe153e3328b36
Sabbbir <sabbir.ahmed1925@gmail.com>
2023-07-29 14:13:14 +0600

    R: 719

---
+++ Important_Pages_on_CodeChef.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Important_Pages_on_CodeChef
+ */
+class Important_Pages_on_CodeChef {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int f, s;
+
+        f = sc.nextInt();
+        s = sc.nextInt();
+
+        
+
+        if(f == 0){
+            System.out.println("https://www.codechef.com/practice");
+        }
+        else if(s == 0) {
+            System.out.println("https://www.codechef.com/contests");
+        }
+        else{
+            System.out.println("https://discuss.codechef.com");
+        
+    }
+}
+    
+}
