1fa394cc1d7c9e1187842fcfded662f52d26f050
Sabbbir <sabbir.ahmed1925@gmail.com>
2023-07-29 13:41:04 +0600

    R: 604

---
+++ Minimum_Number_of_Ones.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Minimum_Number_of_Ones
+ */
+ class Minimum_Number_of_Ones {
+public static void main(String[] args) {
+    
+ 
+        Scanner sc = new Scanner(System.in);
+        int inp, i, f, s;
+        inp = sc.nextInt();
+        for ( i = 0; i < inp; i++){
+            f = sc.nextInt();
+
+
+            s = f/2;
+            System.out.println(s);
+    }
+    }
+}
