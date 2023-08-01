2cc8e58c6799de8399df8c349bae1d4d1afe19cd
Sabbbir <sabbir.ahmed1925@gmail.com>
2023-08-01 17:36:56 +0600

    CodeForces R: 800

---
+++ A_Domino_piling.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * A_Domino_piling
+ */
+public class A_Domino_piling {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inp,i;
+        inp = sc.nextInt();
+        i = sc.nextInt();
+        System.out.println((inp*i)/2);
+    }
+}
