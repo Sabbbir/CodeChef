819df8c658f89682b5d8e3484d6ef782f3bf24ef
Sabbbirr <sabbir.ahmed1925@gmail.com>
2023-08-30 18:56:38 +0600

    Filler

---
+++ A_Sasha_and_Array_Coloring.cpp
@@ -0,0 +1,21 @@
+//    
+//    A_Sasha_and_Array_Coloring.cpp
+//    
+#include<bits/stdc++.h>
+const char nl = '\n';
+using namespace std;
+
+void solve(){
+    int t, count = 0;
+    cin >> t;
+    
+}
+
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
